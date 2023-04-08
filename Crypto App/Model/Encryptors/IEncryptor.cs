b75101e44b53f3f5fe75120dc0fe1d348dead88d
Quaqva <sazonnikoff.sasha@yandex.ru>
2023-04-08 23:06:32 +0400

    feat: add XAML layout

---
+++ Crypto App/Model/Encryptors/IEncryptor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypto_App.Model
+{
+    internal interface IEncryptor<T>
+    {
+        string PrivateKey { get; set; }
+        string PublicKey { get; set; }
+        T Encrypt(T message);
+        T Decrypt(T message);
+    }
+}
