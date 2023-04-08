5c93ddafcb2e64c8ef0ea7d32ceb02ccc3da79ab
Quaqva <sazonnikoff.sasha@yandex.ru>
2023-04-08 19:24:41 +0400

    init: start Crypto App

---
+++ Crypto App/Model/IEncryptor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypto_App.Model
+{
+    internal interface IEncryptor
+    {
+    }
+}
